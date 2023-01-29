b58e594852e8728f8ab6b0a02944f818998282a2
Chris Bloomfield <43499897+ithinkandicode@users.noreply.github.com>
2023-01-29 22:11:57 +0000

    add support for `gain_consumable_heal`, used by stress_eater_effect_2

--- root/mods-unpacked/JuneFurrs-Assassin/extensions/singletons/run_data.gd
+++ root/mods-unpacked/JuneFurrs-Assassin/extensions/singletons/run_data.gd
@@ -22,6 +22,10 @@ func _assassin_add_custom_effects(vanilla_effects:Dictionary)->Dictionary:
 		# All projectiles become homing
 		# Used by: psinas_little_locust
 		"all_homing_projectiles": 0, # int (0 = false, 1 = true)
+
+		# Add "consumable_heal" support to stat_gains_modification_effect.gd
+		# Used by: stress_eater_effect_2
+		"gain_consumable_heal": 0,
 	}
 
 	return Utils.merge_dictionaries(vanilla_effects, custom_effects)
